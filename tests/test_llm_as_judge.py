051a9a39839c656908d05a539c20c77273003720
Jones Agyemang <mightyj@hotmail.co.uk>
2026-05-28 14:32:32 +0100

    Dry up variables

--- tests/test_llm_as_judge.py
+++ tests/test_llm_as_judge.py
@@ -12,11 +12,18 @@ import pytest
 def vcr_config():
     return { "filter_headers": ["authorization"] }
 
+@pytest.fixture
+def judgement_data():
+    inputs = { 'question': 'What is a fish?' }
+    outputs = { 'question_response': 'An animal that lives in water.' }
+    reference_outputs = { 'answer': 'A fish is an aquatic, gill-bearing vetebrate animal.' }
+    return inputs, outputs, reference_outputs
+
 def describe_judgement():
 
     def when_message_body_is_well_formed():
 
-        def judgement_returns_scoring_response():
+        def judgement_returns_scoring_response(judgement_data):
             with patch("src.llm_as_judge.oai_client") as oai_client_mock:
                 mock_llm_response = ns(
                     choices=[
@@ -41,7 +48,7 @@ def describe_judgement():
                     "explanation": "The response is essentially correct and matches the core idea of the answer: a fish is an aquatic animal that lives in water. It omits details like being a vertebrate and gill-bearing, but it is still a good paraphrase."
                 }
 
-                response = judgement(inputs, outputs, reference_outputs)
+                response = judgement(*judgement_data)
 
                 assert response.get('score') == expected_response.get('score')
                 assert response.get('explanation') == expected_response.get('explanation')
@@ -50,17 +57,13 @@ def describe_evaluation():
 
     def when_judgement_succeeds():
         
-        def correctness_evaluator_returns_evaluated_score():
-            inputs = { "question": "What is a fish?" }
-            outputs = { "question_response": "Animal that lives in the sea" }
-            reference_outputs = { "answer": "Foo Bar" }
-
+        def correctness_evaluator_returns_evaluated_score(judgement_data):
             with patch("src.llm_as_judge.judgement") as judgement_mock:
                 judgement_mock.return_value = {
                     "score": 0.1,
                     "explanation": "Not even close!"
                 }
-                evaluation = correctness_evaluator(inputs, outputs, reference_outputs)
+                evaluation = correctness_evaluator(*judgement_data)
 
                 assert evaluation.key == "correctness_evaluator"
                 assert evaluation.score > 0.0
@@ -69,17 +72,13 @@ def describe_evaluation():
 
     def when_judgement_fails():
 
-        def correctness_evaluator_returns_fallback_value():
-            inputs = { "inputs": { "question": "What is a fish?" } }
-            outputs = { "outputs": { "question_response": "Animal that lives in the sea" } }
-            reference_outputs = { "reference_outputs": { "answer": "Foo Bar" } }
-
+        def correctness_evaluator_returns_fallback_value(judgement_data):
             with patch("src.llm_as_judge.judgement") as judgement_mock:
                 judgement_mock.side_effect = RuntimeError("Simulate runtime failure")
 
-                evaluation = correctness_evaluator(inputs, outputs, reference_outputs)
+                evaluation = correctness_evaluator(*judgement_data)
                 assert evaluation.key == "correctness_evaluator"
                 assert evaluation.score == 0.0
                 assert evaluation.comment == "Error whilst evaluating data point"
 
-                judgement_mock.assert_called_once_with(inputs, outputs, reference_outputs)
+                judgement_mock.assert_called_once_with(*judgement_data)
